41b72c72969f289108f03f8b7346728b14e1b497
Andrea Kurra <kouras.andreas@hotmail.com>
2020-01-22 20:44:27 +0200

    Initial Commit.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    void Start()
+    {
+        SetUpBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+        var deltaY = Input.GetAxis("Vertical");
+        deltaX *= Time.deltaTime;
+        deltaX *= moveSpeed;
+        deltaY *= Time.deltaTime;
+        deltaY *= moveSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin,xMax) ;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin,yMax) ;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+   
+    private void SetUpBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+    }
+    
+}
