c038b21278bead7ddefe1ea2f9c356ad83f1d00a
Andrea Kurra <kouras.andreas@hotmail.com>
2020-05-17 18:07:18 +0300

    Floating Damage

---
+++ Assets/Scripts/FloatingText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FloatingText : MonoBehaviour
+{
+    [SerializeField] float destroyTime = 3f;
+    public Vector3 Offset = new Vector3(0, 0.5f, 0);
+    private void Start()
+    {
+        Destroy(gameObject, destroyTime);
+        transform.localPosition += Offset;
+    }
+}
