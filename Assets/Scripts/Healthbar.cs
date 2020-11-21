377c7c6f9567c7cf91da9098928c30d231722bf2
Andreas Kurra <kouras.andreas@hotmail.com>
2020-11-21 20:56:03 +0200

    Change Health Bar to decrease and increase gradually

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -9,7 +9,7 @@ public class Healthbar : MonoBehaviour {
 
     public Gradient gradient;
     public Image fill;
-    public void SetMaxHealth(int health)
+    public void SetMaxHealth(float health)
     {
         slider.maxValue = health;
         slider.value = health;
@@ -17,10 +17,37 @@ public class Healthbar : MonoBehaviour {
         fill.color = gradient.Evaluate(1f);
     }
 
-    public void SetHealth(int health)
+    public void SetHealth(float health)
+     {
+        StartCoroutine(reduceHealthBar(health));
+        //slider.value = health;
+        //fill.color = gradient.Evaluate(slider.normalizedValue);
+        //for health to have a nice effect replaced this with reduceHealthBar
+    }
+
+    private IEnumerator reduceHealthBar(float health)
     {
-        slider.value = health;
-        fill.color = gradient.Evaluate(slider.normalizedValue);
+        if (health < slider.value)
+        {
+            //For health to be decreased 
+            for (float sliderValue = slider.value; health <= sliderValue; sliderValue -= 2)
+            {
+                slider.value = sliderValue;
+                fill.color = gradient.Evaluate(slider.normalizedValue);
+                yield return null;
+            }
+        }
+        else
+        {
+            //For health to be increased 
+            for (float sliderValue = slider.value; sliderValue <= health; sliderValue += 2)
+            {
+                slider.value = sliderValue;
+                fill.color = gradient.Evaluate(slider.normalizedValue);
+                yield return null;
+            }
+        }
+        
     }
 
 }
